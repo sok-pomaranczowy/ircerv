5a57fd9a5954b13f09e52cd4758697c385096029
Jolaa <marchewka.jolanta@gmail.com>
2016-01-23 12:59:07 +0100

    obsluga kolejek

--- src/pl/edu/uksw/irc/queue/EventBus.java
+++ src/pl/edu/uksw/irc/queue/EventBus.java
@@ -1,25 +1,35 @@
 package pl.edu.uksw.irc.queue;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 import pl.edu.uksw.irc.dto.MessageDTO;
 
 /**
  * Created by sok_pomaranczowy on 08.01.16.
  */
 public class EventBus {
-	
+	 
+    ConcurrentLinkedQueue<MessageDTO> incomingQueue;
+    
+    ConcurrentLinkedQueue<MessageDTO> outgoingQueue;
+    
+    public EventBus(){
+        incomingQueue= new ConcurrentLinkedQueue<>();
+        outgoingQueue =new ConcurrentLinkedQueue<>();
+    }
 	public MessageDTO getIncomingEvent(){
 		return null;
 	}
 	
-	public boolean pushOutgoingEvent(){
-		return true;
+	public boolean pushOutgoingEvent(MessageDTO message){
+            boolean success = outgoingQueue.add(message);
+		return success;
 	}
 	
 	public MessageDTO getOutgoingEvent(){
 		return null;
 	}
 	
-	public boolean pushIncomingEvent(){
+	public boolean pushIncomingEvent(MessageDTO message){
 		return true;
 	}
 	
